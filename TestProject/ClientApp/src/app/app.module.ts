8cfe6829955d413af38a2a30c67c491938c3529b
kpapian <karina.papian@stryker.com>
2019-02-08 12:04:26 +0200

    add bootstrap to application

--- TestProject/ClientApp/src/app/app.module.ts
+++ TestProject/ClientApp/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
