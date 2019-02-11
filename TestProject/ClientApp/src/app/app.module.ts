9895e1b1f1cfad57e2819a53b9efa815a0a48472
kpapian <karina.papian@stryker.com>
2019-02-11 22:12:31 +0200

    add basic routing. add main components to application

--- TestProject/ClientApp/src/app/app.module.ts
+++ TestProject/ClientApp/src/app/app.module.ts
@@ -6,17 +6,36 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
+import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { CoachesComponent } from './coaches/coaches.component';
+import { EquipmentComponent } from './equipment/equipment.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { container } from '@angular/core/src/render3';
+
+const appRouts: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'about', component: AboutComponent},
+  { path: 'coaches', component: CoachesComponent},
+  { path: 'equipment', component: EquipmentComponent},
+  { path: 'contacts', component: ContactsComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent,
+    CoachesComponent,
+    EquipmentComponent,
+    ContactsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRouts)
   ],
   providers: [],
   bootstrap: [AppComponent]
