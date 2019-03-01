a09703e27a6a75dffd34761ee243a50e902ed8ed
kpapian <karina.papian@stryker.com>
2019-03-01 15:47:58 +0200

    add Serilog

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,12 +1,12 @@
 using System;
-using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Reflection;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Exceptions;
 
 namespace TestProject
 {
@@ -14,11 +14,44 @@ namespace TestProject
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
-        }
+            string env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production";
+            IConfiguration configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", false, false)
+                .AddJsonFile($"appsettings.{env}.json", true, false)
+                .AddEnvironmentVariables()
+                .Build();
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .Enrich.WithProperty("ApplicationName", Assembly.GetEntryAssembly().GetName().Name)
+                .Enrich.WithProperty(
+                    "ApplicationVersion",
+                    FileVersionInfo.GetVersionInfo(Assembly.GetEntryAssembly().Location).FileVersion)
+                .Enrich.WithExceptionDetails()
+                .Enrich.FromLogContext()
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting Web Host.");
+
+                WebHost.CreateDefaultBuilder(args)
+                    .UseContentRoot(Directory.GetCurrentDirectory())
+                    .UseStartup<Startup>()
+                    .UseConfiguration(configuration)
+                    .UseSerilog()
+                    .Build()
+                    .Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Web Host terminated unexpectedly.");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
     }
 }
--- TestProject/Startup.cs
+++ TestProject/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using TestProject.IServices;
 using TestProject.Services;
 
